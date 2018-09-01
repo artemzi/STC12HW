7c5be33bc9e546fff44ca9b2699e99500f951a88
Artem Zinoviev <arzinoviev@gmail.com>
2018-09-01 12:27:58 +0300

    Task #2 is finished

--- src/com/github/artemzi/hw07/Main.java
+++ src/com/github/artemzi/hw07/Main.java
@@ -19,7 +19,6 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class Main {
     public static void main(String[] args) {
-        // 1.      Создать прокси для класса mathBox (подумайте, что для этого надо добавить к классу)
         Integer[] data = new Integer[1000];
         for (int i = 0; i < 1000; i++) {  // fill data with random ints
             data[i] = ThreadLocalRandom.current().nextInt(1, 100500);
@@ -29,10 +28,9 @@ public class Main {
         Box box = (Box) Proxy.newProxyInstance(MathHandler.class.getClassLoader(), new Class[]{Box.class},
                 handler);
 
-        System.out.printf("[summator] %d%n", box.summator());
-        System.out.println("[splitter] " + box.splitter(123));
+        box.summator();
+        box.splitter(123);
         Random random = new Random();
-        System.out.println("[removeElementIfExists] " + box.removeElementIfExists(data[random.nextInt(100)]));
-        // ==========
+        box.removeElementIfExists(data[random.nextInt(100)]);
     }
 }
