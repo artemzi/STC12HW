1bacef8e137b3533982d0c1df57fede912699024
Artem Zinoviev <arzinoviev@gmail.com>
2018-09-04 12:08:30 +0300

    Add executor from file

--- src/com/github/artemzi/hw08/exec/Executor.java
+++ src/com/github/artemzi/hw08/exec/Executor.java
@@ -3,12 +3,23 @@ package com.github.artemzi.hw08.exec;
 import bsh.EvalError;
 import bsh.Interpreter;
 
+import java.io.IOException;
+
 public interface Executor {
     static void execute(String code) {
         Interpreter interpreter = new Interpreter();
         try {
             interpreter.eval(code);
         } catch (EvalError e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    static void executeFromFile(String filePath) {
+        Interpreter interpreter = new Interpreter();
+        try {
+            interpreter.source(filePath);
+        } catch (IOException | EvalError e) {
             System.err.println(e.getMessage());
         }
     }
