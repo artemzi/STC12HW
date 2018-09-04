ad7c64a59b4e27fa13a9f76debe8a342d2729b0c
Artem Zinoviev <arzinoviev@gmail.com>
2018-09-04 11:41:57 +0300

    Implement Executor

---
+++ src/com/github/artemzi/hw08/exec/Execute.java
@@ -0,0 +1,9 @@
+package com.github.artemzi.hw08.exec;
+
+import static com.github.artemzi.hw08.exec.Executor.execute;
+
+public class Execute {
+    public static void main(String[] args) {
+        execute("System.out.println(\"It works!\")");
+    }
+}
---
+++ src/com/github/artemzi/hw08/exec/Executor.java
@@ -0,0 +1,15 @@
+package com.github.artemzi.hw08.exec;
+
+import bsh.EvalError;
+import bsh.Interpreter;
+
+public interface Executor {
+    static void execute(String code) {
+        Interpreter interpreter = new Interpreter();
+        try {
+            interpreter.eval(code);
+        } catch (EvalError e) {
+            e.printStackTrace();
+        }
+    }
+}
