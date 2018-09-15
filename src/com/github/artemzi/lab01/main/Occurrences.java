462fe1fe4d51c5dfd240edb0bcac68b13adbd6e2
Artem Zinoviev <arzinoviev@gmail.com>
2018-09-15 13:37:56 +0300

    Looks like it works

--- src/com/github/artemzi/lab01/main/Occurrences.java
+++ src/com/github/artemzi/lab01/main/Occurrences.java
@@ -1,11 +1,16 @@
 package com.github.artemzi.lab01.main;
 
 import com.github.artemzi.lab01.content.ContentRequest;
-import com.github.artemzi.lab01.content.ResultProcessor;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.concurrent.*;
+import java.util.logging.Logger;
 
 public class Occurrences implements OccurrencesContact {
+    private static final String FILE_PATH = "data/lab01/";
+    private static final Logger LOGGER = Logger.getLogger(ContentRequest.class.getName());
     private static final int MAX_T = 20;
     private ExecutorService pool;
 
@@ -16,11 +21,21 @@ public class Occurrences implements OccurrencesContact {
     @Override
     public void getOccurrences(String[] sources, String[] words, String res) throws InterruptedException {
         BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+        Thread fileWriter = new Thread(new Thread(() -> {
+            try (PrintWriter writer = new PrintWriter(new File(FILE_PATH + res));) {
+                while (true) {
+                    writer.println(queue.take());
+                }
+            } catch (IOException | InterruptedException e) {
+                LOGGER.warning(e.getMessage());
+            }
+        }));
+        fileWriter.start();
         for (String source : sources) {
             pool.execute(new ContentRequest(source, words, queue));
         }
-        pool.execute(new ResultProcessor(queue, res));
         pool.shutdown();
         pool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        fileWriter.interrupt();
     }
 }
