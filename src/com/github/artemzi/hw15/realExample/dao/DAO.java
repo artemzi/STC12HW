6d8f0c61798621ff300802569ba3ebff071866f1
Artem Zinoviev <arzinoviev@gmail.com>
2018-09-20 16:18:44 +0300

    Refactoring; move logic

--- src/com/github/artemzi/hw15/realExample/dao/DAO.java
+++ src/com/github/artemzi/hw15/realExample/dao/DAO.java
@@ -1,13 +1,44 @@
 package com.github.artemzi.hw15.realExample.dao;
 
-import com.github.artemzi.hw15.realExample.pojo.Student;
+import com.github.artemzi.hw15.realExample.services.FactoryDAO;
 
-public interface DAO {
-    public boolean add(Student student);
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-    public Student getById(int id);
+import static com.github.artemzi.hw15.realExample.services.UtilsDAO.prepareStatement;
 
-    public boolean update(Student student);
+public abstract class DAO<T> {
+    public abstract boolean add(T student);
 
-    public boolean deleteById(int id);
+    public abstract T getById(int id);
+
+    public abstract boolean update(T student);
+
+    public abstract boolean deleteById(int id);
+
+    protected abstract T map(ResultSet resultSet) throws SQLException;
+
+    /**
+     * Method can be used with any sql SELECT query.
+     * just pass required sql and give expected params
+     *
+     * @param sql query
+     * @param values query params
+     * @return Student
+     */
+    protected T find(FactoryDAO connectionManager, String sql, Object... values) {
+        T obj = null;
+        try (Connection connection = connectionManager.getConnection();
+             PreparedStatement statement = prepareStatement(connection, sql, false, values);
+             ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
+                obj = map(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
 }
