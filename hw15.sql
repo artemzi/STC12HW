f2eb73d7e03f1a3e1d9d21a11ab86e711cca28a3
Artem Zinoviev <arzinoviev@gmail.com>
2018-09-20 14:21:51 +0300

    Restrict city removing if student use it

--- hw15.sql
+++ hw15.sql
@@ -13,8 +13,9 @@ CREATE TABLE IF NOT EXISTS students (
     family_name VARCHAR(20) NOT NULL,
 		age INT NOT NULL,
 		contact VARCHAR(50) NULL,
-		city_id INT REFERENCES city (id),
+		city_id INT,
 
     PRIMARY KEY (id),
+		FOREIGN KEY(city_id) REFERENCES city(id) ON DELETE RESTRICT,
     UNIQUE (id)
 );
