65eb96977217a78b094af79d93348031e312f9f1
Artem Zinoviev <arzinoviev@gmail.com>
2018-09-25 18:54:26 +0300

    Remove unused parameters

--- tests/com/github/artemzi/lab01/OccurrencesTest.java
+++ tests/com/github/artemzi/lab01/OccurrencesTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -37,7 +36,7 @@ class OccurrencesTest {
         getResult();
         String content = "";
         try {
-            content = readFile(RESULT_FILE_WITH_PATH, StandardCharsets.UTF_8);
+            content = readFile();
         } catch (IOException e) {
             LOGGER.warning(e.getMessage());
         }
@@ -45,10 +44,10 @@ class OccurrencesTest {
         Assumptions.assumeTrue(content.contains("War".toLowerCase()) | content.contains("Peace".toLowerCase()));
     }
 
-    private static String readFile(String path, Charset encoding) throws IOException {
+    private static String readFile() throws IOException {
         LOGGER.info("Start reading a file...");
-        byte[] encoded = Files.readAllBytes(Paths.get(path));
-        return new String(encoded, encoding);
+        byte[] encoded = Files.readAllBytes(Paths.get(OccurrencesTest.RESULT_FILE_WITH_PATH));
+        return new String(encoded, StandardCharsets.UTF_8);
     }
 
     private void getResult() {
